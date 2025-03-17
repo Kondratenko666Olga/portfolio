4495d918825d56f0cd3f31038567b47f622f84b0
Olga Kondratenko <kondratenko666olga@gmail.com>
2025-03-17 14:20:01 +0200

    start stylishing

--- src/src/components/Header/Header.jsx
+++ src/src/components/Header/Header.jsx
@@ -19,6 +19,45 @@ const Header = () => {
             <a href="#contacts">Contact me</a>
           </span>
         </div>
+        <div className="burger">
+          <svg
+            width="100"
+            height="100"
+            viewBox="0 0 100 100"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="
+    M 72 20
+    A 40 40 0 1 0 28 78
+    M 35 68
+    H 65
+    M 35 75
+    H 65
+    M 35 82
+    H 65
+  "
+              stroke="white"
+              stroke-width="5"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </div>
+
+        <div className="menu-mob">
+          <span className="menu-option">
+            <a href="#about">About me</a>
+          </span>
+          <span className="menu-option">
+            <a href="#services">Services</a>
+          </span>
+          <span className="menu-option">
+            <a href="#contacts">Contact me</a>
+          </span>
+        </div>
       </div>
     </header>
   );
