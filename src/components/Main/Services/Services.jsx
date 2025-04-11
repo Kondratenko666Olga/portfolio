a422190d1fa7f9b94a3e157a231696ba8877dd3e
Olga Kondratenko <kondratenko666olga@gmail.com>
2025-04-11 17:45:15 +0300

    services edited

--- src/components/Main/Services/Services.jsx
+++ src/components/Main/Services/Services.jsx
@@ -3,37 +3,39 @@ import services1 from '../../../images/icons/services1.svg';
 import services2 from '../../../images/icons/services2.svg';
 import services3 from '../../../images/icons/services3.svg';
 
+const servicesData = [
+  {
+    img: services1,
+    alt: 'Software Development icon',
+    title: 'Software Development',
+  },
+  {
+    img: services2,
+    alt: 'Web Development icon',
+    title: 'Web Development',
+  },
+  {
+    img: services3,
+    alt: 'Web Design icon',
+    title: 'Web Design',
+  },
+];
+
 const Services = () => {
   return (
     <section className="services" id="services">
       <div className="container">
-        <h2 className="services-title"></h2>
-        <div className="services-block">
-          <div className="services-block__elem">
-            <img
-              loading="lazy"
-              src={services1}
-              alt="Software Development icon"
-            />
-            <span>
-              Software <br />
-              Development
-            </span>
-          </div>
-          <div className="services-block__elem">
-            <img loading="lazy" src={services2} alt="Web Development icon" />
-            <span>
-              Web <br />
-              Development
-            </span>
-          </div>
-          <div className="services-block__elem">
-            <img loading="lazy" src={services3} alt="Web Design icon" />
-            <span>
-              Web <br />
-              Design
-            </span>
-          </div>
+        <h2 className="title">What i do</h2>
+        <div className="block">
+          {servicesData.map((service, index) => (
+            <div className="elem" key={index}>
+              <img loading="lazy" src={service.img} alt={service.alt} />
+              <span>
+                {service.title.split(' ')[0]} <br />
+                {service.title.split(' ')[1]}
+              </span>
+            </div>
+          ))}
         </div>
       </div>
     </section>
