5b3abbf8926bf4c8ec87ee7db1dcab7a27e0aa41
Olga Kondratenko <kondratenko666olga@gmail.com>
2025-08-25 11:38:12 +0300

    contacts block edited

--- src/components/Main/Contacts/Contacts.jsx
+++ src/components/Main/Contacts/Contacts.jsx
@@ -6,16 +6,22 @@ const Contacts = () => {
       <div className="container">
         <h2 className="contacts-title"></h2>
         <div className="contacts-form">
-          <form action="">
+          <form
+            className="register-form"
+            id="reg_form"
+            name="reg_form"
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
             <div className="input-group">
-              <input type="text" placeholder="Name" />
-              <input type="text" placeholder="Last name" />
+              <input type="text" name="login" placeholder="Name" />
+              <input type="text" name="login" placeholder="Last name" />
             </div>
             <div className="input-group">
-              <input type="text" placeholder="Email" />
-              <input type="text" placeholder="Phone number" />
+              <input type="email" name="email" placeholder="Email" />
+              <input type="phone" name="phone" placeholder="Phone number" />
             </div>
-            <textarea placeholder="Message"></textarea>
             <button type="submit">Submit now</button>
           </form>
         </div>
